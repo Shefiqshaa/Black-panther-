efa7cc672ac640f073691a51e07a5732704ddf2b
Shefiqshaa <90312782+Shefiqshaa@users.noreply.github.com>
2021-09-24 15:58:09 +0530

    Update filter.js

--- plugins/filter.js
+++ plugins/filter.js
@@ -14,7 +14,7 @@ const jid = Config.DISBGM != false ? Config.DISBGM.split(',') : [];
 const Language = require('../language');
 const Lang = Language.getString('filters');
 
-if (Config.WORKTYPE == 'private') {
+if (Config.WORKTYPE == 'public') {
 
 Asena.addCommand({pattern: 'filter ?(.*)', fromMe: true, desc: Lang.FILTER_DESC, dontAddCommandList: true}, (async (message, match) => {
     match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);
